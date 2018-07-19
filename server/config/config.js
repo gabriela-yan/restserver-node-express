bfd96ca9ce0c177d9c8ba7492218364e21d49006
Gabriela <gery.yanez.2828@gmail.com>
2018-07-18 22:30:22 -0500

    Actualiza config

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://coffe_user:Gery312812@ds141641.mlab.com:41641/coffe';
+    urlDB = 'mongodb://coffe_user:password@ds141641.mlab.com:41641/coffe';
 }
 process.env.URLDB = urlDB;
